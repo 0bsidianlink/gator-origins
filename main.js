5333820e94f585c9eaa82fa95e4b1a0382daffdc
0bsidianlink <0bbylink@gmail.com>
2020-10-02 12:40:48 -0600

    commit

--- main.js
+++ main.js
@@ -6,3 +6,20 @@ window.onclick = function () {
     const newColor = Math.floor(Math.random()*16777215).toString(16);
     document.body.style.backgroundColor = `#${newColor}`;
 }
+
+const pokemon = {
+    grovyle: {
+        neutral: 'portraits/grovyle/grovyleneutral.png',
+        smug: 'portraits/grovyle/grovylesmug.png',
+        mute: 'portraits/grovyle/grovylemute.png'
+    },
+    dusknoir: {
+        neutral: '',
+        smug: ''
+    }
+    
+}
+
+function changePFP (emote) {
+    document.getElementById("portrait").src=pokemon.grovyle[emote];
+}
